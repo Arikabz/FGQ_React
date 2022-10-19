54bfb2b17b3eb40ffbb4351310b312bf2de1022c
Arikabz <arturo.guza1999@gmail.com>
2022-10-18 23:03:30 -0500

    first draft of home page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Card from './components/Card'
 
 function App() {
     return (
         <Router>
             <div className="App">
                 <Navbar/>
-                <Hero/>
+                <div className='container mx-auto'>
+                    <Hero/>
+                </div>
                 <Footer/>
             </div>
         </Router>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,33 +1,33 @@
+import Card from './Card'
 
 const Hero = () => {
     return (
-        <div className="container mx-auto">
-            <div className="hero min-h-[80vh] bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
-                            </div>
+        <div className="hero min-h-[80vh] bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+            <Card/>
+                    <h1 className="text-5xl font-bold">&iexcl;Entra ya!</h1>
+                    <p className="py-6">&iexcl;Comienza a ver las predicciones!</p>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Contrase&ntilde;a</span>
+                            </label>
+                            <input type="text" placeholder="Contrase&ntilde;a" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">&iquest;Olvidaste tu contrase&ntilde;a?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Entrar</button>
                         </div>
                     </div>
                 </div>
