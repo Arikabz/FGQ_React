c2053a820b0631adf01fd6feb2119297b9891846
Arikabz <arturo.guza1999@gmail.com>
2023-01-30 18:25:28 -0600

    set stuff to env variables

--- src/auth/auth0-provider-with-history.js
+++ src/auth/auth0-provider-with-history.js
@@ -9,6 +9,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
     const domain = process.env.REACT_APP_AUTH0_DOMAIN;
     const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
     const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
+    const appURL = process.env.REACT_APP_URL;
 
 
 
